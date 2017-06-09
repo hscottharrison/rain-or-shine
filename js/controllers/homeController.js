0006516b8c9cfcde9ad81be0599a5d820ff66aa2
hscottharrison <h.scott.harrison@tcu.edu>
2017-06-09 18:26:56 -0500

    touched up styling. tinkered with slick slider

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,6 +1,6 @@
 angular.module('rainOrShine').controller('homeCtrl', function($scope, weatherService, $stateParams){
 
-weatherService.getWeather($stateParams.state, $stateParams.city)
+  weatherService.getWeather($stateParams.state, $stateParams.city)
     .then(function(response){
       var results = response;
       $scope.location = results.display_location.full;
@@ -11,27 +11,19 @@ weatherService.getWeather($stateParams.state, $stateParams.city)
       $scope.feelslike = results.feelslike_string;
       $scope.icon = results.icon_url;
     })
-    $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-    'WY').split(' ').map(function(state) {
-     return {abbrev: state};
-    });
-weatherService.getHourly($stateParams.state, $stateParams.city)
-.then(function(response){
-  $scope.hourlys = response;
-})
 
-weatherService.getWeekly($stateParams.state, $stateParams.city)
-.then(function(response){
+  weatherService.getHourly($stateParams.state, $stateParams.city)
+    .then(function(response){
+      $scope.hourlys = response;
+    })
 
-  $scope.weeklys = response;
-})
-$scope.radar = 'http://api.wunderground.com/api/7bd6e5a7f2af93d2/animatedradar/animatedsatellite/q/' + $stateParams.state + '/' + $stateParams.city + '.gif?num=6&delay=50&interval=30'
-console.log($scope.radar);
-// weatherService.getRadar()
-// .then(function(response){
-//   console.log(response)
-//   $scope.radar = response;
-// })
+  weatherService.getWeekly($stateParams.state, $stateParams.city)
+    .then(function(response){
+      $scope.weeklys = response;
+    })
+  $scope.radar = 'http://api.wunderground.com/api/7bd6e5a7f2af93d2/animatedradar/animatedsatellite/q/' + $stateParams.state + '/' + $stateParams.city + '.gif?num=6&delay=50&interval=30'
 
-})
+
+
+
+});
